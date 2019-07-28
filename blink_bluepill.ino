78b0d6de917c5c95cbab27b5d21722151ef453a3
m2mlorawan <38450747+m2mlorawan@users.noreply.github.com>
2019-07-28 23:49:34 +0700

    Add files via upload

---
+++ blink_bluepill.ino
@@ -0,0 +1,20 @@
+
+//Black pill
+//#define LED_PIN PB12
+
+//Blue pill
+#define LED_PIN PC13
+
+void setup()
+{
+
+pinMode(LED_PIN, OUTPUT);
+}
+
+void loop()
+{
+digitalWrite(LED_PIN, HIGH);
+delay(1000);
+digitalWrite(LED_PIN, LOW);
+delay(1000);
+}
