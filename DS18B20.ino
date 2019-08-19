6981f603beb5e1d7065c0002e2fd8f97b86b1589
m2mlorawan <38450747+m2mlorawan@users.noreply.github.com>
2019-08-19 18:02:36 +0700

    Add files via upload

---
+++ DS18B20.ino
@@ -0,0 +1,37 @@
+//#include <OneWire.h>
+//Need R 4.7K  Connect Red-Yello Cable
+#include <DallasTemperature.h>
+
+// Data wire is plugged into digital pin B12 on the Maple Mini
+#define ONE_WIRE_BUS PB12
+
+// Setup a oneWire instance to communicate with any OneWire device
+OneWire oneWire(ONE_WIRE_BUS);	
+
+// Pass oneWire reference to DallasTemperature library
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  sensors.begin();	// Start up the library
+  Serial.begin(9600);
+}
+
+void loop(void)
+{ 
+  // Send the command to get temperatures
+  sensors.requestTemperatures(); 
+
+  //print the temperature in Celsius
+  Serial.print("Temperature: ");
+  Serial.print(sensors.getTempCByIndex(0));
+  Serial.print((char)176);//shows degrees character
+  Serial.print("C  |  ");
+  
+  //print the temperature in Fahrenheit
+  Serial.print((sensors.getTempCByIndex(0) * 9.0) / 5.0 + 32.0);
+  Serial.print((char)176);//shows degrees character
+  Serial.println("F");
+  
+  delay(500);
+}
